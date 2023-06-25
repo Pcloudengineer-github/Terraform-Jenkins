2f9084b13ce42784e65720551b7871904e8cb066
Pcloudengineer-github <129958230+Pcloudengineer-github@users.noreply.github.com>
2023-06-25 09:14:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "ap-south-1"  
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
+  ami           = "ami-057752b3f1d6c4d6c" 
+  instance_type = "t3.micro"
   tags = {
       Name = "TF-Instance"
   }
